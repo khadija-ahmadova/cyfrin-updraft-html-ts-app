9d2fd3777b4c964eb743cdc038c683d36b4236da
khadija-ahmadova <xadija.ahmedova@gmail.com>
2025-07-09 01:05:39 +0400

    add fund function

--- index-js.js
+++ index-js.js
@@ -5,6 +5,7 @@ const fundButton = document.getElementById("fundButton");
 const ethAmountInput = document.getElementById("ethAmountInput");
 
 let walletClient
+let publicClient
 
 async function connect() {
     if (typeof window.ethereum !== "undefined") {
@@ -18,5 +19,28 @@ async function connect() {
     }
 }
 
+async function fund() {
+    const ethAmount = ethAmountInput.value
+    console.log("Funding with ${ethAmount} ...")
 
+    if (typeof window.ethereum !== "undefined") {
+        walletClient = createWalletClient({
+            transport: custom(window.ethereum)
+        });
+        await walletClient.requestAddresses();
+        publicClient = createPublicClient({
+            transport: custom(window.ethereum)
+        });
+        await publicClient.simulateContract({
+            address: '',
+            // abi: ,
+            functionName: 'fund',
+            account,
+        })
+    } else {
+        connectButton.innerHTML = "Please install MetaMask"
+    }
+}
+
+fundButton.onclick = fund();
 connectButton.onclick = connect();
